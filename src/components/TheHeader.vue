c748e9619397b6b7bea729a52b3e06b39753cad2
IlyaKZN <ilia.pichugin2014@yandex.ru>
2022-09-18 23:37:10 +0300

    Added base components

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <header class="header">
+    <p class="appName">ALIAS</p>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TheHeader',
+});
+</script>
+
+<style>
+.header {
+  width: 100%;
+  height: 80px;
+  background-color: rgb(252, 208, 11);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-bottom: 2px solid red;
+}
+
+.appName {
+  margin: 0;
+  font-size: 40px;
+  font-weight: 400;
+  color: #ff0000;
+}
+</style>
---
+++ src/components/TheLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="layout">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TheLayout',
+});
+</script>
+
+<style>
+.layout {
+  margin: 0 auto;
+  max-width: 1366px;
+}
+</style>
